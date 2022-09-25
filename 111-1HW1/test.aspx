aa9075a79ff7bb3a612ca02d6d458d5dc11d2896
seanlin0215 <seanlin0215@gmail.com>
2022-09-26 02:35:39 +0800

    0000

---
+++ 111-1HW1/test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="_111_1HW1.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btn_Show" runat="server" Text="Button" OnClick="btn_Show_Click1" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 111-1HW1/test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1HW1
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello App");
+        }
+        protected void btn_Show_Click1(object sender, EventArgs e)
+        {
+            Response.Write("Hello button");
+        }
+    }
+}
